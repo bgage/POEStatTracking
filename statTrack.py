c86b5b2333e36bb335770d66bf8679bc6646f51a
Smelly1sam <swhiteusa@gmail.com>
2017-09-17 15:00:28 -0400

    Class selector menu
    rough menu to select classes

--- statTrack.py
+++ statTrack.py
@@ -3,7 +3,33 @@ stats = configparser.ConfigParser()
 stats.read('stats.txt')
 classes = configparser.ConfigParser()
 classes.read('classes.txt')
-cl = input("Pick a class: ")
+classSelected = 0
+while classSelected == 0:
+	cl = input("Select a number 1 Marauder 2 Ranger 3 Witch 4 Duelist 5 Templar 6 Shadow 7 Scion ")
+	if cl == '1':
+		cl = 'Marauder'
+		classSelected = 1
+	elif cl == '2':
+		cl = 'Ranger'
+		classSelected = 1
+	elif cl == '3':
+		cl = 'Witch'
+		classSelected = 1
+	elif cl == '4':
+		cl = 'Duelist'
+		classSelected = 1
+	elif cl == '5':
+		cl = 'Templar'
+		classSelected = 1
+	elif cl == '6':
+		cl = 'Shadow'
+		classSelected = 1
+	elif cl == '7':
+		cl = 'Scion'
+		classSelected = 1
+#	else:
+#		print "Invalid input"
+
 stats['Base']['class'] = cl
 for atb in classes[cl]:
     print(atb, classes[cl][atb])
