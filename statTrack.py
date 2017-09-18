49043f40d10b4a5f9a39b3c469c748ab8f2f9262
bgage <31483918+bgage@users.noreply.github.com>
2017-09-17 23:59:24 -0400

    Update statTrack.py

--- statTrack.py
+++ statTrack.py
@@ -4,31 +4,18 @@ stats.read('stats.txt')
 classes = configparser.ConfigParser()
 classes.read('classes.txt')
 classSelected = 0
+classChoices = {1: 'Marauder',
+		2: 'Ranger',
+		3: 'Witch',
+		4: 'Duelist',
+		5: 'Templar',
+		6: 'Shadow',
+		7: 'Scion}
 while classSelected == 0:
-	cl = input("Select a number 1 Marauder 2 Ranger 3 Witch 4 Duelist 5 Templar 6 Shadow 7 Scion ")
-	if cl == '1':
-		cl = 'Marauder'
+	classInput = input("Select a number 1 Marauder 2 Ranger 3 Witch 4 Duelist 5 Templar 6 Shadow 7 Scion ")
+	if (int)classInput in range(1,8):
+		cl = classChoices[classInput]
 		classSelected = 1
-	elif cl == '2':
-		cl = 'Ranger'
-		classSelected = 1
-	elif cl == '3':
-		cl = 'Witch'
-		classSelected = 1
-	elif cl == '4':
-		cl = 'Duelist'
-		classSelected = 1
-	elif cl == '5':
-		cl = 'Templar'
-		classSelected = 1
-	elif cl == '6':
-		cl = 'Shadow'
-		classSelected = 1
-	elif cl == '7':
-		cl = 'Scion'
-		classSelected = 1
-#	else:
-#		print "Invalid input"
 
 stats['Base']['class'] = cl
 for atb in classes[cl]:
